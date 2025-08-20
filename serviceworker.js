9df5741c0310ecd991f711bd33867ab97d89348d
Sorro123 <blackassassin2416@gmail.com>
2025-08-20 16:52:18 +0200

    docs: auto-commit and push after build

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 var GHPATH = '/Card-Generator';
 var APP_PREFIX = 'gppwa_';
-var VERSION = 'version_004';
+var VERSION = 'version_002';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
@@ -12,35 +12,40 @@ var CACHE_NAME = APP_PREFIX + VERSION
 self.addEventListener('fetch', function (e) {
   console.log('Fetch request : ' + e.request.url);
   e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
+    caches.match(e.request).then(function (request) {
+      if (request) { 
+        console.log('Responding with cache : ' + e.request.url);
+        return request
+      } else {       
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
     })
-  );
-});
+  )
+})
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('Installing cache : ' + CACHE_NAME);
-      return cache.addAll(URLS);
-    }).then(function() {
-      return self.skipWaiting();
+      return cache.addAll(URLS)
     })
-  );
-});
+  )
+})
 
 self.addEventListener('activate', function (e) {
   e.waitUntil(
-    Promise.all([
-      self.clients.claim(),
-      caches.keys().then(function (keyList) {
-        return Promise.all(keyList.map(function (key) {
-          if (key.startsWith(APP_PREFIX) && key !== CACHE_NAME) {
-            console.log('Deleting old cache : ' + key);
-            return caches.delete(key);
-          }
-        }));
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
       })
-    ])
-  );
-});
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
